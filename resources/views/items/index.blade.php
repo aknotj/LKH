2b70b279a4128819dba7a110d964aceb792d08b9
aknotj <akaneotsuji08@gmail.com>
2022-07-23 02:43:08 +0000

    Created items index view

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-  <p>
-    Content
-    {{$message}}
-  </p>
+  <table>
+    <tr>
+      <th>Item Id</th>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Price</th>
+      <td>Units in stock</td>
+      <td></td>
+    </tr>
+    @foreach($items as $item)
+      <tr>
+        <td>{{$item->id}}</td>
+        <td>{{$item->name}}</td>
+        <td>{{$item->description}}</td>
+        <td>{{$item->price}}</td>
+        <td>{{$item->stock}} pc</td>
+        <td><a href="/items/{{$item->id}}">Details</a></td>
+      </tr>
+    @endforeach
+  </table>
 @endsection
