ddba5169aa71392ef22940cd7acb118e52755721
aknotj <akaneotsuji08@gmail.com>
2022-07-28 00:16:37 +0000

    Added products edit layouts

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,41 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="create-products">
-    <div class="form">
+  <div class="products_create">
+    <div class="heading">
+      <h2>Edit Product</h2>
+    </div>
+    <div class="product_form">
       <form action="/products/{{$product->id}}" method="POST">
         {{ csrf_field() }}
-        <div class="input-form">
+        <div class="input_form">
           <label for="name">Name</label>
-          <input name="name" value="{{$product->name}}">
+          <input type="text" name="name" value="{{$product->name}}">
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="description">Description</label>
           <textarea name="description">{{$product->description}}</textarea>
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="price">Price</label>
-          <input name="price" value="{{$product->price}}">
+          <input type="text" name="price" value="{{$product->price}}">USD
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="stock">Inventory Unit</label>
-          <input name="stock" value="{{$product->stock}}">
+          <input type="text" name="stock" value="{{$product->stock}}">Units
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="stock">Category</label>
-          <input name="category" value="{{$product->stock}}">
+          <input type="text" name="category" value="{{$product->stock}}">
         </div>
-        <div class="input-form">
-          <input type="hidden" name="_method" value="patch">
-          <input type="submit" value="Update">
+        <div class="actions">
+          <div class="update">
+            <input type="hidden" name="_method" value="patch">
+            <input type="submit" value="Update">
+          </div>
+          <div class="delete">
+            <form action="/products/{{$product->id}}" method="POST">
+              {{ csrf_field() }}
+              <input type="hidden" name="_method" value="delete">
+              <input type="submit" name="" value="Delete" class="delete-btn">
+            </form>
+          </div>
         </div>
       </form>
     </div>
   </div>
 
-  <form action="/products/{{$product->id}}" method="POST">
-    {{ csrf_field() }}
-    <input type="hidden" name="_method" value="delete">
-    <input type="submit" name="" value="Delete">
-  </form>
 @endsection
