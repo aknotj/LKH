93b5bf9aaa91e513d1c9a6cc427d5d0440372533
aknotj <akaneotsuji08@gmail.com>
2022-07-29 06:03:14 +0000

    Created products create views

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="create-products">
-    <div class="form">
-      <form action="/products" enctype="multipart/form-data" method="POST">
+  <div class="products_create">
+    <div class="heading">
+      <h2>Edit Product</h2>
+    </div>
+    <div class="product_form">
+      <form action="/products" method="POST">
         {{ csrf_field() }}
-        
-        <div class="input-form">
+        <div class="input_form">
           <label for="name">Name</label>
-          <input name="name">
+          <input type="text" name="name">
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="description">Description</label>
           <textarea name="description"></textarea>
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="price">Price</label>
-          <input name="price">
+          <input type="text" name="price">USD
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="stock">Inventory Unit</label>
-          <input name="stock">
+          <input type="text" name="stock">Units
         </div>
-        <div class="input-form">
+        <div class="input_form">
           <label for="stock">Category</label>
-          <input name="category">
+          <input type="text" name="category">
         </div>
-        <div class="input-form">
-          <label for="stock">Image</label>
-          <input type="file" name="img_path">
-        </div>
-        <div class="input-form">
-          <input type="submit" value="Submit">
+        <div class="actions">
+          <div class="update">
+            <input type="hidden" name="_method" value="patch">
+            <input type="submit" value="Submite">
+          </div>
         </div>
       </form>
     </div>
