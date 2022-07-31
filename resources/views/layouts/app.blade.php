80dc1c2b9fc6078c0200ac83ccff8217b866bc0e
aknotj <akaneotsuji08@gmail.com>
2022-07-31 02:59:36 +0000

    Changed title and header logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LKH</title>
+        <title>det Lille Lyserødt Kaffehus</title>
         <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}">
     </head>
     <body class="antialiased">
         <header>
             <nav>
-                <a href="/" class="header_logo">det Lille Lyserødt Hus</a>
+                <a href="/" class="header_logo">det Lille Lyserødt Kaffehus</a>
                 <ul class="header_menu">
                     <li><a href="">About</a></li>
                     <li><a href="/products">Products</a></li>
